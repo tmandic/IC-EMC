519265aa80e3428d3d7acfce78a409de64682124
tmandic <tvrtko.mandic@gmail.com>
2016-05-10 18:27:40 +0200

    Add files via upload

---
+++ gpib_instrument.m
@@ -0,0 +1,46 @@
+classdef gpib_instrument
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        address = 18
+        vendor = 'ni'
+        handle
+        buffer_size = 1044480
+        boardindex = 0;
+    end
+    
+    methods
+        %% Constructor
+        function this_gpib_instrument = gpib_instrument(inaddress, invendor, inbuffer_size, inboardindex)
+            if nargin == 4
+                this_gpib_instrument.address = inaddress;
+                this_gpib_instrument.vendor = invendor;
+                this_gpib_instrument.buffer_size = inbuffer_size;
+                this_gpib_instrument.boardindex = inboardindex;
+            end             
+        end
+        %% open gpib port
+        function this_gpib_instrument = open(this_gpib_instrument)
+            this_gpib_instrument.handle = gpib(this_gpib_instrument.vendor,this_gpib_instrument.boardindex,this_gpib_instrument.address);    
+            this_gpib_instrument.handle.InputBufferSize = this_gpib_instrument.buffer_size;
+            fopen(this_gpib_instrument.handle);
+            if strcmp('open', this_gpib_instrument.handle.status)  
+                disp('gpib_instrument: GPIB port open');     
+            elseif strcmp('closed', this_gpib_instrument.handle.status)             
+                disp('gpib_instrument: Unable to open GPIB port');     
+            end
+        end
+        %% close gpib port
+        function this_gpib_instrument = close(this_gpib_instrument)
+            fclose(this_gpib_instrument.handle);
+            if strcmp('open', this_gpib_instrument.handle.status)  
+                disp('gpib_instrument: Unable to close GPIB port');     
+            elseif strcmp('closed', this_gpib_instrument.handle.status)             
+                disp('gpib_instrument: GPIB port closed');     
+            end            
+        end        
+    end
+    
+end
+
